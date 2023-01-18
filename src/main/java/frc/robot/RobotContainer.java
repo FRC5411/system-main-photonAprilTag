085e6e7401bd9b45e736632a789fac2ffc171921
AnshulBompada <115746973+AnshulBompada@users.noreply.github.com>
2023-01-17 18:56:06 -0600

    Added distance setups

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,8 +1,10 @@
 package frc.robot;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -18,10 +20,11 @@ public class RobotContainer {
   PhotonPipelineResult result;
   double yaw;
   drive tankDrive;
-  PIDController pid;
+  PIDController angle_pid;
+  PIDController move_pid;
   CommandXboxController controller;
   Trigger A;
-//  NetworkTableInstance instance;
+  Trigger B;
 
   public RobotContainer() {
     controller = new CommandXboxController(0);
@@ -30,8 +33,10 @@ public class RobotContainer {
     result = new PhotonPipelineResult();
     target = new PhotonTrackedTarget();
     A = controller.a();
+    B = controller.b();
 
-    pid = new PIDController(0.03, 0, 0.025);
+    angle_pid = new PIDController(0.03, 0, 0.025);
+    move_pid = new PIDController(0.03, 0, 0.025);
 
     tankDrive.setDefaultCommand(new DefaultDrive(() -> controller.getLeftY(),
      () -> controller.getRightX(), 
@@ -41,8 +46,11 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    A.onTrue(new DefaultDrive(() -> 0, () -> pid.calculate(april_tag(), yaw), tankDrive));
+    A.onTrue(new DefaultDrive(() -> 0, () -> angle_pid.calculate(april_tag(), 0), tankDrive));
     A.onFalse(new DefaultDrive(() -> 0, () -> 0, tankDrive));
+
+    B.onTrue(new DefaultDrive(() -> 0, () -> move_pid.calculate(april_tagr(), 0.05), tankDrive));
+    B.onFalse(new DefaultDrive(() -> 0, () -> 0, tankDrive));
   }
 
   public double april_tag() {
@@ -51,6 +59,20 @@ public class RobotContainer {
       SmartDashboard.putBoolean("Has Targets", result.hasTargets());
       target = result.getBestTarget();
       return target.getYaw();
+    }
+    return 0.0;
+  }
+
+  public double april_tagr() {
+    double height = Units.inchesToMeters(24);
+    double target = Units.inchesToMeters(22.5);
+    double angle = 1.4056;
+    result = camera.getLatestResult();
+    if(result.hasTargets() == true){
+      SmartDashboard.putBoolean("Has Targets", result.hasTargets());
+      double dist = PhotonUtils.calculateDistanceToTargetMeters(height, target, angle, 
+      Units.degreesToRadians(result.getBestTarget().getPitch()));
+      return dist;
     }
     return 0.0;
   }
