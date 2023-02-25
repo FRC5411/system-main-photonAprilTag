7424852d54d2956ea46e90098f01774203a74e68
AnshulBompada <115746973+AnshulBompada@users.noreply.github.com>
2023-02-25 10:56:15 -0600

    UPDATED TO CAN

--- src/main/java/frc/robot/Subsystems/drive.java
+++ src/main/java/frc/robot/Subsystems/drive.java
@@ -1,36 +1,44 @@
 package frc.robot.Subsystems;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class drive extends SubsystemBase {
-  PWMVictorSPX TopLeft;
-  PWMVictorSPX TopRight;
-  PWMVictorSPX BottomLeft;
-  PWMVictorSPX BottomRight;
+  WPI_TalonSRX TopLeft;
+  WPI_TalonSRX TopRight;
+  WPI_TalonSRX BottomLeft;
+  WPI_TalonSRX BottomRight;
   MotorControllerGroup Right;
   MotorControllerGroup Left;
   DifferentialDrive Tank_Drive;
   AHRS m_gyro;
 
   public drive(AHRS gyro) {
-    TopLeft = new PWMVictorSPX(4);
-    TopRight = new PWMVictorSPX(1);
-    BottomLeft = new PWMVictorSPX(2);
-    BottomRight = new PWMVictorSPX(3);
+    TopLeft = new WPI_TalonSRX(4);
+    TopRight = new WPI_TalonSRX(1);
+    BottomLeft = new WPI_TalonSRX(2);
+    BottomRight = new WPI_TalonSRX(3);
 
     m_gyro = gyro;
-
+/*
     Left = new MotorControllerGroup(TopLeft, BottomLeft);
-    Right = new MotorControllerGroup(TopRight, BottomRight);
+    Right = new MotorControllerGroup(TopRight, BottomRight);*/
+    TopLeft.follow(BottomLeft);
+    TopRight.follow(BottomRight);
+
+    TopLeft.setNeutralMode(NeutralMode.Brake);
+    TopRight.setNeutralMode(NeutralMode.Brake);
+    BottomLeft.setNeutralMode(NeutralMode.Brake);
+    BottomRight.setNeutralMode(NeutralMode.Brake);
 
     Right.setInverted(true);
 
-    Tank_Drive = new DifferentialDrive(Left, Right);
+    Tank_Drive = new DifferentialDrive(BottomLeft, BottomRight);
   }
 
   public void stop() {
